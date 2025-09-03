52a42c0697065cd35b6893b02399ef08073a9d78
Seo0800 <jisung9581@gmail.com>
2025-09-03 12:33:47 +0900

    Update main_05_ui_LCEL.py

--- main_05_ui_LCEL.py
+++ main_05_ui_LCEL.py
@@ -19,7 +19,7 @@ import streamlit as st
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+from langchain_chroma.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 
 from langchain_core.prompts import PromptTemplate
@@ -191,4 +191,5 @@ if st.button("질문하기"):
 # with st.expander("디버그 정보"):
 #     st.write("persist_dir:", st.session_state.persist_dir)
 #     st.write("vectorstore 생성됨:", st.session_state.vectorstore is not None)
+
 
